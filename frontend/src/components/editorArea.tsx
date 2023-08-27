85d13a2198d738ee79d2618833b723fd74cf8675
cu8code <git8ankanroy@gmail.com>
2023-08-28 02:23:52 +0530

    increased the EditorArea width for better UI

--- frontend/src/components/editorArea.tsx
+++ frontend/src/components/editorArea.tsx
@@ -5,7 +5,7 @@ import { ThemeToggleButton } from "./themeToggleButton";
 
 export function EditorArea() {
     return (
-        <div className='w-1/2 h-full rounded-md flex flex-col overflow-hidden bg-white border p-1'>
+        <div className='w-2/3 h-full rounded-md flex flex-col overflow-hidden bg-white border p-1'>
             <div className="flex flex-row h-10 items-center border justify-between p-1">
                 <div>Editor</div>
                 <div className="flex flex-row gap-4">
--- frontend/src/components/questionBar.tsx
+++ frontend/src/components/questionBar.tsx
@@ -7,7 +7,7 @@ export function OutputandInputArea() {
     const { inputContext, setInputContext } = useInputContext()
     const { outputContext } = useOutputContext()
     return (
-        <div className='flex flex-col w-1/2 bg-slate-50 h-full p-1'>
+        <div className='flex flex-col w-1/3 bg-slate-50 h-full p-1'>
             <div className="flex flex-col h-2/3 border-t-slate-50 border-r-slate-50 border-l-slate-50 border-b-slate-950 border p-1 whitespace-pre-line">
                 <div>Output</div>
 
